0c02d535b9a025926dd369ba4dcabb5812eafa97
aimee <heymihan89@gmail.com>
2024-07-09 19:13:09 +0900

    chore: 파일 주석 추가

--- transformJsonToTs.js
+++ transformJsonToTs.js
@@ -89,7 +89,9 @@ fs.readFile(absolutePath, "utf8", (err, data) => {
   // TypeScript 파일에 쓰기
   fs.writeFile(
     filePath,
-    `${tokenStrings.join("").replace(/"__(.*?)__"/g, "$1")}`,
+    `// !이 파일은 자동 생성된 파일이니까 수기로 수정하지 마세요! 임의로 수정 노노!
+
+${tokenStrings.join("").replace(/"__(.*?)__"/g, "$1")}`,
     (writeErr) => {
       if (writeErr) {
         console.error("Error writing TypeScript file:", writeErr);
