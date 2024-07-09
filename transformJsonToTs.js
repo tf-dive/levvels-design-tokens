cc11ad818e3d917cb15fd6c185da98cdcd6db196
aimee <heymihan89@gmail.com>
2024-07-09 19:39:06 +0900

    chore: 테스트 2

--- transformJsonToTs.js
+++ transformJsonToTs.js
@@ -89,8 +89,7 @@ fs.readFile(absolutePath, "utf8", (err, data) => {
   // TypeScript 파일에 쓰기
   fs.writeFile(
     filePath,
-    `
-// !이 파일은 자동 생성된 파일이니까 수기로 수정하지 마세요! 임의로 수정 노노!
+    `const a = 1;
 
 ${tokenStrings.join("").replace(/"__(.*?)__"/g, "$1")}`,
     (writeErr) => {
